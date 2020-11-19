e8e2f7ce2151a250e032f716d2b63f1aabf79cbb
mohammadhasan3 <mohamed.hassan940@hotmail.com>
2020-11-19 18:43:55 +0300

    Started linking work with backend!

--- src/stores/gameStore.js
+++ src/stores/gameStore.js
@@ -4,6 +4,7 @@ import games from "../games";
 //Libraries
 import { action, makeObservable, observable } from "mobx";
 import slugify from "react-slugify";
+import axios from "axios";
 
 class GameStore {
   games = games;
@@ -14,8 +15,14 @@ class GameStore {
       createGame: action,
       deleteGame: action,
       updateGame: action,
+      fetchGames: action,
     });
   }
+
+  fetchGames = async () => {
+    const response = await axios.get("http://localhost:8000/games");
+    this.games = response.data;
+  };
 
   createGame = (newGame) => {
     newGame.id = games[games.length - 1].id + 1;
@@ -36,4 +43,5 @@ class GameStore {
 }
 
 const gameStore = new GameStore();
+gameStore.fetchGames();
 export default gameStore;
